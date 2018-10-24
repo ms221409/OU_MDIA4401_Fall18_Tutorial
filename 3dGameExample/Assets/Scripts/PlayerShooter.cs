2b9aa0a2c7412d4bde32fd1f03157c58a2ee700a
Michael Stephens <ms221409@ohio.edu>
2018-10-24 14:44:03 -0400

    4.0
    Signed-off-by: Michael Stephens <ms221409@ohio.edu>

---
+++ 3dGameExample/Assets/Scripts/PlayerShooter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerShooter : MonoBehaviour 
+{
+	public GameObject bulletPrefab;
+	public Transform bulletSourceTransform;
+	public int ammoRemaining = 10;
+	public Text ammoLabel;
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			if (ammoRemaining > 0)
+				ShootBullet ();
+		}
+	}
+
+
+	void ShootBullet ()
+	{
+		GameObject thisBullet = Instantiate (bulletPrefab, bulletSourceTransform.position, Quaternion.identity);
+		thisBullet.transform.localEulerAngles = transform.localEulerAngles;
+
+		ammoRemaining -= 1;
+		ammoLabel.text = ammoRemaining.ToString ();
+	}
+
+
+	public void RefillAmmo (int ammoAmount)
+	{
+		ammoRemaining += 4;
+		ammoLabel.text = ammoRemaining.ToString ();
+	}
+}
---
+++ 3dGameExample/Assets/Scripts/Projectile/AmmoBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AmmoBox : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider other)
+	{
+		other.transform.parent.GetComponent <PlayerShooter> ().RefillAmmo (4);
+		Destroy (transform.parent.gameObject);
+	}
+}
